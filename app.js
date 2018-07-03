e4048132a3072e20bb50721b728b7c20f2111841
Bill Buff <bbuff27@gmail.com>
2018-07-02 22:44:30 -0600

    Add initial routes/views

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const request = require("request");
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Everything works well!");
+  res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+  const campgrounds = [
+    {name: "Bartlett Cove", image:"https://s3-production.bobvila.com/slides/26527/widened/bartlett-cove.jpg?1526489504"},
+    {name: "Redfish Lake", image:"https://s3-production.bobvila.com/slides/26528/widened/redfish.jpg?1526489504"},
+    {name: "Blackwoods Campground", image:"https://s3-production.bobvila.com/slides/26529/widened/blackwoods.jpg?1526489505"}
+  ];
+  res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.listen(3000, () => {
